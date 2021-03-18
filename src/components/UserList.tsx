766e7038bd6d03afbcecea3f448e84fd0dfea943
arthurseredaa <neonbetcy@gmail.com>
2021-03-18 14:30:59 +0200

    Fetch users in user list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
-export const UserList = () => {
-  const users = useTypedSelector(state => state.user.users);
-  console.log(users);
+export const UserList: React.FC = () => {
+  const { users, error, loading } = useTypedSelector(state => state.user),
+    {fetchUsers} = useActions();
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
+  if(error) {
+    return <p>{error}</p>
+  }
 
   return (
     <div>
       UserList
+        {
+          users.map(user => <p key={user.id}>{user.name}</p>)
+        }
     </div>
   )
 };
