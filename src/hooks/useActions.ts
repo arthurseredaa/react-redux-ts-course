2d61d59dea6da77cd40404f3f954cd3ab33128a3
arthurseredaa <neonbetcy@gmail.com>
2021-03-18 14:31:48 +0200

    Add comment

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux"
 import { bindActionCreators } from "redux";
 import ActionCreators from "../store/actions";
 
+// Позволяет использовать actions без диспатча
 export const useActions = () => {
   const dispatch = useDispatch();
   return bindActionCreators(ActionCreators, dispatch);
