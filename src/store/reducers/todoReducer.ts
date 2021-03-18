fc77baf5fbc1728b7b046b6c628d0d16183f5ad8
arthurseredaa <neonbetcy@gmail.com>
2021-03-18 14:32:48 +0200

    Create todoReducer

---
+++ src/store/reducers/todoReducer.ts
@@ -0,0 +1,32 @@
+import { ITodoState, TodoAction, TodoActionTypes } from './../../types/todo';
+
+const initialState: ITodoState = {
+  todos: [],
+  loading: false,
+  error: null,
+  page: 1,
+  limit: 5,
+}
+
+export const TodoReducer = (state: ITodoState = initialState, action: TodoAction): ITodoState => {
+  switch (action.type) {
+    case TodoActionTypes.FETCH_TODOS:
+      return {
+        ...state, loading: true
+      }
+    case TodoActionTypes.FETCH_TODOS_FAILURE:
+      return {
+        ...state, loading: false, error: action.payload
+      }
+    case TodoActionTypes.FETCH_TODOS_SUCCESS:
+      return {
+        ...state, loading: false, todos: action.payload
+      }
+    case TodoActionTypes.SET_TODO_PAGE:
+      return {
+        ...state, page: action.payload
+      }
+    default:
+      return state;
+  }
+}
