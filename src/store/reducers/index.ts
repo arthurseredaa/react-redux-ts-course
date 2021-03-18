ac291d8f4697dc442fee97a7c425067b9bbb4f22
arthurseredaa <neonbetcy@gmail.com>
2021-03-18 13:32:18 +0200

    Create root reducer and type of root reducer

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,6 @@
+import { userReducer } from './userReducer';
+import { combineReducers } from "redux";
+
+export const rootReducer = combineReducers({user: userReducer});
+
+export type RootState = ReturnType<typeof rootReducer>
