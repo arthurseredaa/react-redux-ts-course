7eca2fa15700ac4cc8897d0c36535b493d8c0006
arthurseredaa <neonbetcy@gmail.com>
2021-03-18 14:32:40 +0200

    Add todoReducer to rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,6 +1,7 @@
+import { TodoReducer } from './todoReducer';
 import { userReducer } from './userReducer';
 import { combineReducers } from "redux";
 
-export const rootReducer = combineReducers({user: userReducer});
+export const rootReducer = combineReducers({user: userReducer, todo: TodoReducer});
 
 export type RootState = ReturnType<typeof rootReducer>
